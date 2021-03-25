ff952bb33176e7df6d688dcd251227e75cc5bb0b
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-25 18:18:45 +0100

    Reformat template

--- flake.nix
+++ flake.nix
@@ -32,8 +32,8 @@
           announce-drone = pkgs.writeScriptBin "announce-drone" ''
             #!${pkgs.stdenv.shell}
             tokenHeader="Token: $TOKEN"
-            echo "{ \"drone\": \"[$DRONE_REPO - $DRONE_COMMIT_REF] $DRONE_BUILD_STATUS: \
-            $DRONE_COMMIT_MESSAGE - $DRONE_COMMIT_AUTHOR $DRONE_BUILD_EVENT $DRONE_BUILD_LINK\"}" | \
+            echo "{ \"drone\": \"[$DRONE_COMMIT_REF@$DRONE_REPO] $DRONE_BUILD_STATUS: \
+            $DRONE_COMMIT_MESSAGE - ($DRONE_BUILD_EVENT by $DRONE_COMMIT_AUTHOR) $DRONE_BUILD_LINK\"}" | \
             ${curl}/bin/curl -s -H "Content-Type: application/json" -H "$tokenHeader" -X POST --data-binary @- 127.0.0.1:8989/webhook
           '';
 
