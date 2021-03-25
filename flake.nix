e8cc65942cc0b3a3d0aaeff384f5cac5922fcd7d
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-25 17:33:10 +0100

    Add token

--- flake.nix
+++ flake.nix
@@ -34,10 +34,10 @@
             tokenHeader="Token: $TOKEN"
 
 
-            echo "${curl}/bin/curl -s -H \"Content-Type: application/json\" -H \"Token: $TOKEN\" -X POST --data-binary @- 127.0.0.1:8989/webhook"
+            # echo "${curl}/bin/curl -s -H \"Content-Type: application/json\" -H \"Token: $TOKEN\" -X POST --data-binary @- 127.0.0.1:8989/webhook"
 
 
-            echo "{ \"type\": \"drone\", \"data\": \"[$DRONE_REPO - $DRONE_COMMIT_REF] $DRONE_BUILD_STATUS: \
+            echo "{ \"drone\": \"[$DRONE_REPO - $DRONE_COMMIT_REF] $DRONE_BUILD_STATUS: \
             $DRONE_COMMIT_MESSAGE - $DRONE_COMMIT_AUTHOR $DRONE_BUILD_EVENT $DRONE_BUILD_LINK\"}" | \
             ${curl}/bin/curl -s -H "Content-Type: application/json" -H "$tokenHeader" -X POST --data-binary @- 127.0.0.1:8989/webhook
           '';
