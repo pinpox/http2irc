8cd77951a07cc03a27e5d3f106ec82dc248ccee8
Pablo Ovelleiro Corral <github@pablo.tools>
2021-03-25 17:06:51 +0100

    Add token

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
             #!${pkgs.stdenv.shell}
             echo "[$DRONE_REPO - $DRONE_COMMIT_REF] $DRONE_BUILD_STATUS: \
             $DRONE_COMMIT_MESSAGE - $DRONE_COMMIT_AUTHOR $DRONE_BUILD_EVENT $DRONE_BUILD_LINK" | \
-            ${curl}/bin/curl -H "Content-Type: application/json" -H "Token: test" -X POST --data-binary @- 127.0.0.1:8989/webhook
+            ${curl}/bin/curl -H "Content-Type: application/json" -H "Token: TOKEN" -X POST --data-binary @- 127.0.0.1:8989/webhook
           '';
 
           announce-test = pkgs.writeScriptBin "announce-test" ''
